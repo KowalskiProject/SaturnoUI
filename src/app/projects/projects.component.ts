4433b63895735960bdf9e81b074505d07cb05a16
renatopoulicer <renatopoulicer@gmail.com>
2017-12-29 12:32:29 -0200

    Added basic page for project already integrated with a model for projects, missing only the service

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Project } from '../../models/project';
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-
-  constructor() { }
+  projects:  Project[] = [];
+  constructor() {
+    for(let i=0;i<5;i++) {
+      this.projects.push(new Project('Project Title', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'));
+    }
+    }
 
   ngOnInit() {
   }
