ae4e8340eb66f3de8e4587e9b0bc8364c6440c30
renatopoulicer <renatopoulicer@gmail.com>
2017-12-29 19:55:39 -0200

    made progress in create project dialog

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Project } from '../../models/project';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
@@ -8,12 +10,14 @@ import { Project } from '../../models/project';
 export class ProjectsComponent implements OnInit {
   projects:  Project[] = [];
   constructor() {
-    for(let i=0;i<5;i++) {
+    for(let i=0; i<3; i++) {
       this.projects.push(new Project('Project Title', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'));
     }
     }
 
   ngOnInit() {
   }
-
+  teste(){
+    console.log('teste');
+  }
 }
